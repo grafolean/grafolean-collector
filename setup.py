6ea453478adb97c3e61c5adaa799dc2d6f337633
Anze <info@grafolean.com>
2019-11-04 21:17:07 +0100

    Include requirements in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
+import os
 from setuptools import setup, find_packages
+
+requirements_txt = os.path.dirname(os.path.realpath(__file__)) + '/requirements.txt'
+with open(requirements_txt, "rt") as f:
+    install_requires = f.read().splitlines()[1:]
 
 setup(name='grafoleancollector',
       version='@@VERSION@@',
@@ -9,4 +14,5 @@ setup(name='grafoleancollector',
       description='Common utilities for creating (controlled) collectors for Grafolean',
       packages=find_packages(exclude=['tests']),
       long_description=open('README.md').read(),
+      install_requires=install_requires,
       zip_safe=False)
